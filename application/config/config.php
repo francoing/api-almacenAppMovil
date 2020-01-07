3cef8e072b577e14a8b663277278c3bd8e692636
francoing <franco.montti.19@gmail.com>
2020-01-07 00:59:46 -0300

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://almacen-api.herokuapp.com/rest/index.php';
+$config['base_url'] = 'https://almacen-api.herokuapp.com/index.php';
 
 /*
 |--------------------------------------------------------------------------
